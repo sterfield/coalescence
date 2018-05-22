e9b1a786fc7cd42785afca50a1ee3a3c67cccca7
Sterfield <sterfield@gmail.com>
2018-05-22 17:44:40 +0200

    Update setup.py and gitignore

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from setuptools import setup, find_packages
 setup(
     name="coalescence",
     version="0.1",
+    author="Guillaume Loetscher",
+    author_email="sterfield@gmail.com",
+    description="Merge several data source with ease !",
+    url="https://github.com/sterfield/coalescence",
     packages=find_packages(),
-    test_require=['pytest', 'pytest-mock'],
+    tests_require=['pytest'],
     setup_requires=['pytest-runner'],
 )
